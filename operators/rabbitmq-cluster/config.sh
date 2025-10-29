939ba149e8a12e55e763a914037aa2a312f805a6
MarcoTulioGT <devopsgt@gmail.com>
2025-10-28 18:43:51 -0600

    add rabbitmq cluster

--- operators/rabbitmq-cluster/config.sh
+++ operators/rabbitmq-cluster/config.sh
@@ -1,2 +1,11 @@
 default_user_rI80HjMBlewqMMI7Uac
 EMy6SHXwV2lkLRjdJXJWBrO4BZ4TKbx8
+
+
+username="$(kubectl get secret rabbitmq-cluster-default-user -n messaging -o jsonpath='{.data.username}' | base64 --decode)"
+echo "username: $username"
+
+password="$(kubectl get secret rabbitmq-cluster-default-user -n messaging -o jsonpath='{.data.password}' | base64 --decode)"
+echo "password: $password"
+default_user_vpNq_Oo_0_KcXH8e-QN
+nV7wuB_LqHUkPVpmL_WIoFyn6D2PZiLi
